7f20f79e8ee8e7c3b51881faaf595c10f70cc039
royalblue <glwingstne@gmail.com>
2023-01-16 00:35:06 +0300

    Health

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,5 +3,5 @@ import requests
 api_url = 'http://localhost:8000'
 
 def test_healthcheck():
-    response = requests.get('http://localhost:8000/v1/products')
+    response = requests.get('http://localhost:8000/__health')
     assert response.status_code == 200
