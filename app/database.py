a750ebb4c6cdbefcdef579f9c57ae9b22ed5584d
royalblue <glwingstne@gmail.com>
2023-01-16 00:34:45 +0300

    Health

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@postgres:5432/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@db:5432/postgres"
 # SQLALCHEMY_DATABASE_URL = "postgresql://diana:password@172.17.0.2:5432/microdb"
 
 
--- app/main.py
+++ app/main.py
@@ -3,19 +3,40 @@ from sqlalchemy.orm import Session
 from .database import get_db
 from . import crud, models, schemas
 from .database import engine
+from . import crud, models, schemas
+from .database import engine
 
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+@app.post("/v1/products")
+def create(details: schemas.CreateProduct, db: Session = Depends(get_db)):
+    to_create = models.Product(
+        name = details.name,
+        price = details.price,
+        shopid = details.shopid
+    )
+    db.add(to_create)
+    db.commit()
+    return {
+        "success": True,
+        "created_id":to_create.id
+    }
+
 @app.get("/v1/products")
-async def read_products():
-    return "test"
+async def read_products(db: Session = Depends(get_db), skip: int = 0, limit: int = 100):
+    products = crud.get_products(db, skip=skip, limit=limit)
+    return products
 
 
+@app.get("/v1/products/{id}")
+async def read_product_by_id(id: int, db: Session = Depends(get_db)):
+    result = crud.get_products_by_id(db, id = id)
+    if result is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return result
 
 @app.get("/__health")
 async def check_service():
     return
-
-
-
